9c69281ecfba2d637f7b0e47a3863b165fa238cb
Huynh Minh Man <huynhman2908@gmail.com>
2023-08-24 01:14:15 +0700

    edit item cart

--- asset/js/controller.js
+++ asset/js/controller.js
@@ -31,37 +31,25 @@ export let renderHTML = (list) => {
     let { id, name, price, screen, backCamera, frontCamera, img, desc, type } =
       item;
     let contentTr = `
-    <div class="col-6 col-md-4 col-xl-3 mb-2">
-      <div class="card animate__animated animate__backInUp animate__delay-3s">
-        <img class="card-img-top" src="${img}" alt="${name}">
-        <div class="card-body">
-          <h6 class="card-title text-center">${name}</h6>
-          <p class="card-text text-danger text-center">${price}<sup><u>$</u></sup></p>
-        </div>
-        <div class="text-right">
-            <a class="btn btn-sm btn-danger">
-                <i class="fa fa-shopping-cart text-white" aria-hidden="true"></i>
-            </a>
-            <a class="btn btn-sm btn-outline-primary" onclick="chiTietSanPham('${id}')">
-                <i class="fa fa-book text-primary" aria-hidden="true"></i>
-            </a>
+      <div class="col-6 col-md-4 col-xl-3">
+        <div class="card animate__animated animate__backInUp animate__delay-3s">
+          <img class="card-img-top" src="${img}" alt="${name}">
+          <div class="card-body">
+            <h6 class="card-title text-center">${name}</h6>
+            <p class="card-text text-danger text-center">${price}<sup><u>$</u></sup></p>
+          </div>
+          <div class="text-right p-2">
+              <button class="btn btn-danger">
+                  <i class="fa fa-shopping-cart text-white" aria-hidden="true"></i>
+              </button>
+              <button class="btn btn-outline-primary">
+                  <i class="fa fa-book text-primary" aria-hidden="true"></i>
+              </button>
+          </div>
         </div>
       </div>
-    </div>
     `;
     createHTML += contentTr;
   });
   document.getElementById("contents-items").innerHTML = createHTML;
 };
-
-// function chiTietSanPham(id) {
-//   let item = dssp.find((x) => x.id == id);
-//   modalTitle.innerHTML = item.name;
-//   let html = `
-//       <img src="${item.img}" class="img-fluid">
-//       <p class="text-danger text-center mt-2">Giá Bán: ${item.price}
-//       )} <sup><u>$</u></sup></p>
-//   `;
-//   modalBody.innerHTML = html;
-//   showModal.click();
-// }
--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,11 @@
 import { fetchProduct } from "./controller.js";
 
 fetchProduct();
+
+window.showCart = () => {
+  document.getElementById("cart_show").style.display = "flex";
+};
+
+window.outCart = () => {
+  document.getElementById("cart_show").style.display = "none";
+};

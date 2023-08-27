17dcff24e3dfd9b4dc6d268df256d5ef2c1397a2
Huynh Minh Man <huynhman2908@gmail.com>
2023-08-27 19:46:13 +0700

    final

--- homepage/js/main.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelectorAll('.item');
-
-items.forEach(item => {
-    item.addEventListener('click', () => {
-        const link = item.querySelector('a').getAttribute('href');
-        window.location.href = link;
-    });
-});
-
